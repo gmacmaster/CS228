20d727d53eb752e85529a2650ff2235a6bc33ac7
Gordon MacMaster <gmacmast@uvm.edu>
2019-08-28 08:42:44 -0400

    added window files

---
+++ pygameWindow.py
@@ -0,0 +1,15 @@
+import pygame
+
+pygame.init()
+
+
+class PYGAME_WINDOW:
+    def __init(self):
+        self.screen = pygame.display.set_mode((300, 100))
+
+    def Prepare(self):
+        pygame.event.get()
+        self.screen.fill(...)
+
+    def Reveal(self):
+        pass
